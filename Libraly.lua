908bcc041afd5762dfb083c214e9c06f0e664700
XIRZEHUB <142651691+XIRZEHUB@users.noreply.github.com>
2023-09-03 13:23:35 +0700

    Update Libraly.lua

--- Libraly.lua
+++ Libraly.lua
@@ -30,9 +30,9 @@ local Library = {
     HudRegistry = {};
 
     FontColor = Color3.fromRGB(255, 255, 255);
-    MainColor = Color3.fromRGB(28, 28, 28);
-    BackgroundColor = Color3.fromRGB(20, 20, 20);
-    AccentColor = Color3.fromRGB(0, 85, 255);
+    MainColor = Color3.fromRGB(30, 30, 30);
+    BackgroundColor = Color3.fromRGB(28, 28, 28);
+    AccentColor = Color3.fromRGB(0, 209, 255);
     OutlineColor = Color3.fromRGB(50, 50, 50);
     RiskColor = Color3.fromRGB(255, 50, 50),
 
